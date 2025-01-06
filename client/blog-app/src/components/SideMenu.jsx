19b6153efc6283f0474e5aa0706626ab4bdd65d0
wachira <wachirastudeo@gmail.com>
2025-01-06 07:14:44 +0700

    side menu search

--- client/blog-app/src/components/SideMenu.jsx
+++ client/blog-app/src/components/SideMenu.jsx
@@ -1,6 +1,26 @@
+import { Link, useSearchParams } from "react-router-dom";
 import Search from "./Search";
 
 const SideMenu = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const handleFilterChange = (e) => {
+    if (searchParams.get("sort") !== e.target.value) {
+      setSearchParams({
+        ...Object.fromEntries(searchParams.entries()),
+        sort: e.target.value,
+      });
+    }
+  };
+  const handleCategoryChange = (category) => {
+    if (searchParams.get("cat") !== category) {
+      setSearchParams({
+        ...Object.fromEntries(searchParams.entries()),
+        cat: category,
+      });
+    }
+  };
+
   return (
     <div className="px-4 h-max sticky top-8">
       <h1 className="mb-4 text-sm font-medium">Search</h1>
@@ -11,8 +31,9 @@ const SideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="newest"
-            className="appearance-none w-4 h-4 border-[1.5px] border-sky-600 cursor-pointer rounded-sm bg-white checked:bg-sky-600"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
           />
           Newest
         </label>
@@ -20,8 +41,9 @@ const SideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="popular"
-            className="appearance-none w-4 h-4 border-[1.5px] border-sky-600 cursor-pointer rounded-sm bg-white checked:bg-sky-600"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
           />
           Most Popular
         </label>
@@ -29,8 +51,9 @@ const SideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="trending"
-            className="appearance-none w-4 h-4 border-[1.5px] border-sky-600 cursor-pointer rounded-sm bg-white checked:bg-sky-600"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
           />
           Trending
         </label>
@@ -38,22 +61,54 @@ const SideMenu = () => {
           <input
             type="radio"
             name="sort"
+            onChange={handleFilterChange}
             value="oldest"
-            className="appearance-none w-4 h-4 border-[1.5px] border-sky-600 cursor-pointer rounded-sm bg-white checked:bg-sky-600"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
           />
           Oldest
         </label>
       </div>
       <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
       <div className="flex flex-col gap-2 text-sm">
-        <span className="underline cursor-pointer">All</span>
-        <span className="underline cursor-pointer">Web Design</span>
-        <span className="underline cursor-pointer">Development</span>
-        <span className="underline cursor-pointer">Databases</span>
-        <span className="underline cursor-pointer">Search Engines</span>
-        <span className="underline cursor-pointer">Marketing</span>
+        <span
+          className="underline cursor-pointer"
+          onClick={() => handleCategoryChange("general")}
+        >
+          All
+        </span>
+        <span
+          className="underline cursor-pointer"
+          onClick={() => handleCategoryChange("web-design")}
+        >
+          Web Design
+        </span>
+        <span
+          className="underline cursor-pointer"
+          onClick={() => handleCategoryChange("development")}
+        >
+          Development
+        </span>
+        <span
+          className="underline cursor-pointer"
+          onClick={() => handleCategoryChange("databases")}
+        >
+          Databases
+        </span>
+        <span
+          className="underline cursor-pointer"
+          onClick={() => handleCategoryChange("seo")}
+        >
+          Search Engines
+        </span>
+        <span
+          className="underline cursor-pointer"
+          onClick={() => handleCategoryChange("marketing")}
+        >
+          Marketing
+        </span>
       </div>
     </div>
   );
 };
+
 export default SideMenu;
