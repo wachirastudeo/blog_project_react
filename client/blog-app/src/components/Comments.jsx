bed8921288a4cd43ff36ac455a1af149bc2b9001
wachira <wachirastudeo@gmail.com>
2025-01-19 07:34:37 +0700

    comments

--- client/blog-app/src/components/Comments.jsx
+++ client/blog-app/src/components/Comments.jsx
@@ -1,22 +1,33 @@
-import { format } from "timeago.js";
-import Image from "./Image";
-import { useAuth, useUser } from "@clerk/clerk-react";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { toast } from "react-toastify";
 import axios from "axios";
+import Comment from "./Comment";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { toast } from "react-toastify";
 
-const Comment = ({ comment, postId }) => {
+const fetchComments = async (postId) => {
+  const res = await axios.get(
+    `${import.meta.env.VITE_API_URL}/comments/${postId}`
+  );
+  return res.data;
+};
+
+const Comments = ({ postId }) => {
   const { user } = useUser();
   const { getToken } = useAuth();
-  const role = user?.publicMetadata?.role;
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["comments", postId],
+    queryFn: () => fetchComments(postId),
+  });
 
   const queryClient = useQueryClient();
 
   const mutation = useMutation({
-    mutationFn: async () => {
+    mutationFn: async (newComment) => {
       const token = await getToken();
-      return axios.delete(
-        `${import.meta.env.VITE_API_URL}/comments/${comment._id}`,
+      return axios.post(
+        `${import.meta.env.VITE_API_URL}/comments/${postId}`,
+        newComment,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -26,43 +37,65 @@ const Comment = ({ comment, postId }) => {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["comments", postId] });
-      toast.success("Comment deleted successfully");
     },
     onError: (error) => {
       toast.error(error.response.data);
     },
   });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+
+    const data = {
+      desc: formData.get("desc"),
+    };
+
+    mutation.mutate(data);
+  };
+
   return (
-    <div className="p-4 bg-slate-50 rounded-xl mb-8">
-      <div className="flex items-center gap-4">
-        {comment.user.img && (
-          <Image
-            src={comment.user.img}
-            className="w-10 h-10 rounded-full object-cover"
-            w="40"
-          />
-        )}
-        <span className="font-medium">{comment.user.username}</span>
-        <span className="text-sm text-gray-500">
-          {format(comment.createdAt)}
-        </span>
-        {user &&
-          (comment.user.username === user.username || role === "admin") && (
-            <span
-              className="text-xs text-red-300 hover:text-red-500 cursor-pointer"
-              onClick={() => mutation.mutate()}
-            >
-              delete
-              {mutation.isPending && <span>(in progress)</span>}
-            </span>
+    <div className="flex flex-col gap-8 lg:w-3/5 mb-12">
+      <h1 className="text-xl text-gray-500 underline">Comments</h1>
+      <form
+        onSubmit={handleSubmit}
+        className="flex items-center justify-between gap-8 w-full"
+      >
+        <textarea
+          name="desc"
+          placeholder="Write a comment..."
+          className="w-full p-4 rounded-xl"
+        />
+        <button className="bg-blue-800 px-4 py-3 text-white font-medium rounded-xl">
+          Send
+        </button>
+      </form>
+      {isPending ? (
+        "Loading..."
+      ) : error ? (
+        "Error loading comments!"
+      ) : (
+        <>
+          {mutation.isPending && (
+            <Comment
+              comment={{
+                desc: `${mutation.variables.desc} (Sending...)`,
+                createdAt: new Date(),
+                user: {
+                  img: user.imageUrl,
+                  username: user.username,
+                },
+              }}
+            />
           )}
-      </div>
-      <div className="mt-4">
-        <p>{comment.desc}</p>
-      </div>
+
+          {data.map((comment) => (
+            <Comment key={comment._id} comment={comment} postId={postId} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
 
-export default Comment;
+export default Comments;
